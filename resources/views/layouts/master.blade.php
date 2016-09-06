1282a5c561da6e05a0bf0c25d4a3f1e52c2d19e7
Mercile55 <mercile55@yandex.ru>
2016-09-06 09:21:49 +0300

    master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="{{ @yield('keywords') }}">
-    <meta name="description" content="{{ @yield('description') }}">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="description" content="@yield('description')">
     <title>@yield('title')</title>
 
     <!-- Bootstrap -->
